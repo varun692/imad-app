7366cfb58938a445ca717ea009bf0aab47d8f5f7
varun692 <30685029+varun692@users.noreply.github.com>
2017-08-04 09:41:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res) {
+app.get('/varun', function (req, res) {
   res.send('Article one requested and will be served here');
 });
 
-app.get('/article-two', function (req, res) {
+app.get('/Hashini', function (req, res) {
   res.send('Article two requested and will be served here');
 });
 
-app.get('/article-three', function (req, res) {
+app.get('/Edric', function (req, res) {
   res.send('Article three requested and will be served here');
 });
 
